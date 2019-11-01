3860e59ccc38bc9cbed0f7e4233ad765dfd0b81a
Erin Deuel <end.gaia@gmail.com>
2019-11-01 15:05:57 -0700

    add more animals

--- Models/AnimalShelterContext.cs
+++ Models/AnimalShelterContext.cs
@@ -22,7 +22,7 @@ namespace AnimalShelter.Models
                     Gender = "Female",
                     Age = 2,
                     KennelNumber = "7E",
-                    Description = "Carla is sassy and love big Open spaces. She is the leader of the group."
+                    Description = "Carla is sassy and loves big Open spaces. She is the leader of the al-Pack-as."
                 },
 
                 new ShelterList
@@ -54,14 +54,76 @@ namespace AnimalShelter.Models
                 {
                     ShelterListId = 4,
                     Name = "Toad",
-                    PhotoPath = "wwwroot/uploads/Toad.jpg",
+                    PhotoPath = "wwwroot/uploads/King.jpg",
                     AnimalType = "Dog",
                     Breed = "Chinese Crested",
                     Gender = "Female",
                     Age = 1,
                     KennelNumber = "3D",
                     Description = "Toad is a fashionista. If she isn't wearing a cute outfit, she shakes uncontrollably from her naked rage."
+                },
+                
+                new ShelterList
+                {
+                    ShelterListId = 5,
+                    Name = "Raggedy Anne",
+                    PhotoPath = "wwwroot/uploads/Anne.jpg",
+                    AnimalType = "Cat",
+                    Breed = "Ragdoll",
+                    Gender = "Female",
+                    Age = 3,
+                    KennelNumber = "3B",
+                    Description = "Raggedy Anne is a very relaxed. Her favorite pastime is sleeping and getting attention."
+                },
+                new ShelterList
+                {
+                    ShelterListId = 6,
+                    Name = "Blood",
+                    PhotoPath = "wwwroot/uploads/Blood.jpg",
+                    AnimalType = "Bird",
+                    Breed = "Red Factor Canary",
+                    Gender = "Male",
+                    Age = 1,
+                    KennelNumber = "1B",
+                    Description = "Loves heavy metal music. You must give him the seed or he will fight you."
+                },
+                new ShelterList
+                {
+                    ShelterListId = 7,
+                    Name = "King Arthor",
+                    PhotoPath = "wwwroot/uploads/Toad.jpg",
+                    AnimalType = "Monkey",
+                    Breed = "Emperor Tamerin",
+                    Gender = "Male",
+                    Age = 12,
+                    KennelNumber = "7G",
+                    Description = "King Arthor believes He is king of all animals. Will steal your heart and your Acai fruit."
+                },
+                new ShelterList
+                {
+                    ShelterListId = 8,
+                    Name = "Crab",
+                    PhotoPath = "wwwroot/uploads/Crab.jpg",
+                    AnimalType = "Crab",
+                    Breed = "Chesapeake blue Crab",
+                    Gender = "Male",
+                    Age = 5,
+                    KennelNumber = "3F",
+                    Description = "Always Angry."
+                },
+                new ShelterList
+                {
+                    ShelterListId = 9,
+                    Name = "Blubbles",
+                    PhotoPath = "wwwroot/uploads/Blub.jpg",
+                    AnimalType = "Fish",
+                    Breed = "Paedocypris progenetica",
+                    Gender = "Female",
+                    Age = 20,
+                    KennelNumber = "2F",
+                    Description = "So tiny, you can barely see her. Blubbles Just likes doing her own thing."
                 }
+                
             );
         }
     }
