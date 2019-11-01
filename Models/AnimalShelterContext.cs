839e1b2f8b94c6147dc864a0fc28d32928c6c185
Erin Deuel <end.gaia@gmail.com>
2019-11-01 10:23:51 -0700

    add to ShelterList

--- Models/AnimalShelterContext.cs
+++ Models/AnimalShelterContext.cs
@@ -1,6 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace AnimalShelter.Model
+namespace AnimalShelter.Models
 {
-
+    public class AnimalShelterContext : DbContext
+    {
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+        }
+            public DbSet<ShelterList> ShelterList { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ShelterList>().HasData(
+            );
+        }
+    }
 }
---
+++ Models/ShelterList.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelter.Models
+{
+    public class ShelterList
+    {
+        public int AnimalId {get; set;}
+        [Required]
+        [StringLength(20)]
+        public string Name {get; set;}
+        [Required]
+        [StringLength(20)]
+        public string AnimalType {get; set;}
+        [Required]
+        [StringLength(30)]
+        public string Breed {get; set;}
+        [Required]
+        public int Age {get; set;}
+        [Required]
+        [StringLength(15)]
+        public string Kennelnumber {get; set;}
+        [Required]
+        [StringLength(500)]
+        public string Description {get; set;}
+        
+
+    }
+}
